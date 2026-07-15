611155ca6c5d2278062654a63497d49605360901
jean150506 <jeanp.viana@gmail.com>
2026-07-14 21:07:36 -0300

    add: added new treat for uppercase

--- data_cleanning/cleanning_job.py
+++ data_cleanning/cleanning_job.py
@@ -15,14 +15,12 @@ class treatments:
         print("depois:", list(df.columns))
         df.columns = [c.replace(" ","")if isinstance(c,str) else c for c in df.columns]
         return df
-    # def treating_spaces(df):
-    #     for col in df.columns:
-    #         print(col)
-    #         df = df.rename(columns={col:col.strip() for col in df.columns})
-    #         # if df[col].dtype == "object":
-    #         # df[col] = df[col].astype("string").str.strip()
-    #         #     # df[col] = df[col].str.replace(";", ",", regex=False)
-        # return df
+    @staticmethod
+    def treating_uppercase(df: pd.DataFrame) -> pd.DataFrame:
+        df.columns = [c.upper() if isinstance(c,str) else c for c in df.columns]
+        return df
+
+   
     
     @staticmethod
     def normalizing_gender(df: pd.DataFrame):
@@ -36,28 +34,15 @@ class treatments:
         if "AGE" in df.columns:
             print("true")
 
-        # if "AGE" in df.columns:
-        #     print("column dtype:", df["AGE"].dtype)
-        #     types = df["AGE"].apply(lambda x: type(x).__name__)
-        #     for t in types:
-        #         print(t)                       # imprime um tipo por linha
-        #     print("counts:\n", types.value_counts())
-        #     df["AGE_numeric"] = pd.to_numeric(df["AGE"], errors="coerce")
-        # return df
-        
-        #     df["AGE"] = df["AGE"].astype("string").str.replace("NaN","18")
-
-        # return df
 
 
 
 
 df = treatments.reading_file()
 result = treatments.treating_spaces(df)
-print(result)
-# result_v2 = treatments.normalizing_gender(result)
-# print(result_v2.columns)
-# age_treatment = treatments.normalizing_age(result_v2)
-# print(age_treatment)
+treating_uppercase = treatments.treating_uppercase(result)
+print(treating_uppercase)
+
+
 
 
